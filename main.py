3c7c7b9fa5dff7cf96905ed22523b6f6fd7ef6c7
ehsan gheychisaz <ehsan.ghechisaz82@gmail.com>
2024-07-10 11:05:21 +0330

    update template

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, EmailStr
-from jinja2 import Environment, FileSystemLoader
 import aiosmtplib
 from email.message import EmailMessage
 from fastapi.responses import RedirectResponse
@@ -17,27 +16,33 @@ class EmailSchema(BaseModel):
     subject: str
     body: dict
 
+def read_html_template(file_path: str) -> str:
+    with open(file_path, 'r') as file:
+        template = file.read()
+    return template
 
 
 async def send_email(email: EmailSchema):
-    env = Environment(loader=FileSystemLoader('.'))
+    # print(email.body)
+     
     if email.body['type'] == 1:
-        template = env.get_template('templates/greeting.html')
-        template.render(name=email.body['name'])
-    if email.body['type'] == 2:
-        template = env.get_template('templates/news.html')
-        template.render(news=email.body['news'])
-    if email.body['type'] == 3:
-        template = env.get_template('templates/verify.html')
-        template.render(code=email.body['code'])
-
-
+        template = read_html_template('templates/greeting.html')
+        html_content = template.replace('{{ name }}', email.body['name'])
+    elif email.body['type'] == 2:
+        template = read_html_template('templates/news.html')
+        html_content = template.replace('{{ news }}', email.body['news'])
+    elif email.body['type'] == 3:
+        template = read_html_template('templates/verify.html')
+        html_content = template.replace('{{ code }}', email.body['code'])
+    else:
+        raise HTTPException(status_code=400, detail="Invalid email body type")
+    
     message = EmailMessage()
     message["From"] = "your_email@example.com"
     # message["To"] = email.email
     message["Subject"] = email.subject
     # message.set_content(email.body)
-    message.add_alternative(template, subtype='html')
+    message.add_alternative(html_content, subtype='html')
 
     try:
         for i in email.email:
